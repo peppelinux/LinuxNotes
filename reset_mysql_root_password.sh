ac367349e8061c204f930b04882bc7554fda6b6c
Giuseppe De Marco <demarcog83@gmail.com>
2017-10-13 12:46:35 +0200

    Update reset_mysql_root_password.sh

--- reset_mysql_root_password.sh
+++ reset_mysql_root_password.sh
@@ -17,4 +17,12 @@ SET PASSWORD FOR 'root'@'localhost' = PASSWORD('new_password');
 # IF "ERROR 1131 (42000): You are using MariaDB as an anonymous user and anonymous users are not allowed to modify user settings"
 UPDATE mysql.user SET authentication_string = PASSWORD('new_password') WHERE User = 'root' AND Host = 'localhost';
 
+# localhost keyword is reserved for connection using the MySQL socket and you should use the ip-address 127.0.0.1 for TCP connections to the MySQL network port on 127.0.0.1. This means that both the server must grant privileges to users from specifically 127.0.0.1, and the client must use -h 127.0.0.1 to go through the tunnel instead of connecting to a local socket.
+GRANT ALL PRIVILEGES on *.* to 'root'@'127.0.0.1' IDENTIFIED BY 'new_password';
+
+# keep in mind to not use 'localhost' but 127.0.0.1 in client connection
+ssh server.hostname.com -L 1234:127.0.0.1:3306
+mysql -h 127.0.0.1 -P1234 -u root -p
+
 FLUSH PRIVILEGES;
+FLUSH QUERY CACHE;
